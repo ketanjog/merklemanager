cd39d358e503c6922d84f6ae56cea2ee70387f8d
Soham Jog <jogsoham2003@gmail.com>
2024-05-08 16:54:21 +0530

    types edit 1

--- types.ts
+++ types.ts
@@ -9,10 +9,12 @@
 export type Tree = {
   nodes: string[];
   metadata: string;
+  index: number;
 };
 export type Project = {
   author: string;
   title: string;
+  counter: number;
   tree: Tree[];
   changelog: string;
 };
